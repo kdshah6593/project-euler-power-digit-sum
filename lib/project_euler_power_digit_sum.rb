04039481ac3cd4f6b50f34bc581b11f40a684d5a
Kunal <kdshah6593@gmail.com>
2021-08-24 14:45:40 -0400

    completed solution, all tests passing

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,8 @@
 # code your solution here
+def power_digit_sum(base, exponent)
+    number = base**exponent
+  
+    arr = number.to_s.split("").map{|x| x.to_i}
+  
+    arr.reduce{|sum, n| sum + n}
+  end
